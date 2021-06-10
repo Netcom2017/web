511247da697f068a8903a8ec3bd18aada5da7c70
Galib77 <razagalib7@gmail.com>
2021-06-11 02:46:05 +0530

    added header footer

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -1,122 +1,166 @@
-import React from 'react';
-import FooterBottom from './FooterBottom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
+import React from "react";
+import FooterBottom from "./FooterBottom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
 const Footer = () => {
-
-    return (
-        <footer>
-            <div className="footer-area grey-bg pt-80 pb-30">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-xl-3 col-lg-3 col-md-4">
-                            <div className="footer-wrapper mb-30">
-                                <div className="footer-logo">
-                                    <a href="index.html"><img src="assets/img/logo/logo.png" alt="" /></a>
-                                </div>
-                                <div className="footer-text">
-                                    <p>At vero eoset accusamus et iusto odio dignissimos ducimus qui blpraesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi </p>
-                                </div>
-                                <div className="footer-icon">
-                                    <a href="#"><i><FontAwesomeIcon icon={['fab', 'facebook-f']} /></i></a>
-                                    <a href="#"><i><FontAwesomeIcon icon={['fab', 'twitter']} /></i></a>
-                                    <a href="#"><i><FontAwesomeIcon icon={['fab', 'instagram']} /></i></a>
-                                    <a href="#"><i><FontAwesomeIcon icon={['fab', 'youtube']} /></i></a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-xl-2 col-lg-2 col-md-4">
-                            <div className="footer-wrapper mb-30">
-                                <h4 className="footer-title">Our Services</h4>
-                                <ul className="fotter-menu">
-                                    <li><a href="#">User Strategy </a></li>
-                                    <li><a href="#">Product Designing </a></li>
-                                    <li><a href="#">Marketing Strategy</a></li>
-                                    <li><a href="#">IT Consultancy</a></li>
-                                    <li><a href="#">Server Security</a></li>
-                                    <li><a href="#">Product Marketing</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div className="col-xl-2 col-lg-2 col-md-4">
-                            <div className="footer-wrapper mb-30">
-                                <h4 className="footer-title">Quick Links</h4>
-                                <ul className="fotter-menu">
-                                    <li><a href="#">About Us</a></li>
-                                    <li><a href="#">Need a Consultant?</a></li>
-                                    <li><a href="#">Our Services</a></li>
-                                    <li><a href="#">Have Any Questions?</a></li>
-                                    <li><a href="#">Meet Our Team</a></li>
-                                    <li><a href="#">Contact Us</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div className="col-xl-3 col-lg-3 col-md-6">
-                            <div className="footer-wrapper mb-30">
-                                <h4 className="footer-title">Recent News</h4>
-                                <div className="footer-news">
-                                    <ul>
-                                        <li>
-                                            <div className="footer-news-img">
-                                                <a href="#"><img src="assets/img/footer/01.jpg" alt="" /></a>
-                                            </div>
-                                            <div className="footer-news-text">
-                                                <h5><a href="#">Building Real Time Charts With Grap HQL & Postgres</a></h5>
-                                            </div>
-                                        </li>
-                                        <li>
-                                            <div className="footer-news-img">
-                                                <a href="#"><img src="assets/img/footer/02.jpg" alt="" /></a>
-                                            </div>
-                                            <div className="footer-news-text">
-                                                <h5><a href="#">How To Build An Endless Runner Or Virtual Reality</a></h5>
-                                            </div>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-xl-2 col-lg-2 col-md-6">
-                            <div className="footer-wrapper mb-30">
-                                <h4 className="footer-title">Contact Us</h4>
-                                <div className="footer-info">
-                                    <p>But I must explain to you
-                                        how all this mistaken</p>
-                                </div>
-                                <ul className="contact-link">
-                                    <li>
-                                        <div className="contact-address-icon">
-                                            <i className="far fa-phone"></i>
-                                        </div>
-                                        <div className="contact-address-text">
-                                            <h4>+812  (345) 778 88</h4>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div className="contact-address-icon">
-                                            <i className="far fa-envelope-open"></i>
-                                        </div>
-                                        <div className="contact-address-text">
-                                            <h4>support@gmail.com</h4>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div className="contact-address-icon">
-                                            <i className="far fa-map-marker-alt"></i>
-                                        </div>
-                                        <div className="contact-address-text">
-                                            <h4>227 Marion Street, Columbia</h4>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
+  return (
+    <footer>
+      <div className="footer-area grey-bg pt-80 pb-30">
+        <div className="container">
+          <div className="row">
+            <div className="col-xl-3 col-lg-3 col-md-4 text-center">
+              <div className="footer-wrapper mb-30">
+                <div className="footer-logo">
+                  <a href="index.html">
+                    <img
+                      src="assets/img/logo/logo.png"
+                      alt="netcom-business-solution-logo"
+                    />
+                  </a>
                 </div>
-                <FooterBottom />
+                <div className="footer-icon">
+                  <h5 className="text-center">Follow Us</h5>
+                  <a href="https://www.facebook.com/netcombusinesssolutions/">
+                    <i>
+                      <FontAwesomeIcon icon={["fab", "facebook-f"]} />
+                    </i>
+                  </a>
+                  <a href="https://www.linkedin.com/company/netcombusinesssolutions">
+                    <i>
+                      <FontAwesomeIcon icon={["fab", "linkedin"]} />
+                    </i>
+                  </a>
+                  <a href="https://www.instagram.com/netcomindia/">
+                    <i>
+                      <FontAwesomeIcon icon={["fab", "instagram"]} />
+                    </i>
+                  </a>
+                </div>
+                <div className="google-partner mt-3">
+                  <img
+                    src="assets/img/logo/google.png"
+                    alt="netcom-business-solution-logo"
+                  />
+                </div>
+              </div>
             </div>
-        </footer>
-    );
-}
+            <div className="col-xl-3 col-lg-3 col-md-3 col-sm-12">
+              <div className="footer-wrapper mb-30">
+                <h4 className="footer-title">Our Services</h4>
+                <ul className="fotter-menu">
+                  <li>
+                    <Link href="/coming-soon" as="/coming-soon">
+                      Website Development
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/coming-soon" as="/coming-soon">
+                      E-Commerce Solutions
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/coming-soon" as="/coming-soon">
+                      Applications Development
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/coming-soon" as="/coming-soon">
+                      Digital Marketing
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/coming-soon" as="/coming-soon">
+                      UI/UX Designing
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/coming-soon" as="/coming-soon">
+                      Other Services
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-xl-3 col-lg-3 col-md-3 col-sm-12">
+              <div className="footer-wrapper mb-30">
+                <h4 className="footer-title">Our Solutions</h4>
+                <ul className="fotter-menu">
+                  <li>
+                    <Link href="/coming-soon" as="/coming-soon">
+                      Industry Solutions
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/coming-soon" as="/coming-soon">
+                      Integrated Solutions
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/coming-soon" as="/coming-soon">
+                      CRM Solution
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/coming-soon" as="/coming-soon">
+                      SaaS Solution
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/coming-soon" as="/coming-soon">
+                      Software Solution
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/coming-soon" as="/coming-soon">
+                      Case Studies
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-xl-3 col-lg-3 col-md-3 col-sm-12">
+              <div className="footer-wrapper mb-30">
+                <h4 className="footer-title">Contact Us</h4>
+                <ul className="contact-link">
+                  <li>
+                    <div className="contact-address-icon">
+                      <i className="far fa-phone"></i>
+                    </div>
+                    <div className="contact-address-text">
+                      <a href="tel:917620665635">+91-76206 65635</a>
+                    </div>
+                  </li>
+                  <li>
+                    <div className="contact-address-icon">
+                      <i className="far fa-envelope-open"></i>
+                    </div>
+                    <div className="contact-address-text">
+                      <a href="mailto:sales@netcom-india.com">
+                        sales@netcom-india.com
+                      </a>
+                    </div>
+                  </li>
+                  <li>
+                    <div className="contact-address-icon">
+                      <i className="far fa-map-marker-alt"></i>
+                    </div>
+                    <div className="contact-address-text">
+                      <h4>
+                        Office No 201 & 202, Premlok Complex, Near HP Petrol
+                        Pump, Above Banana Leaf Restaurant, Visharant Wadi,
+                        Pune, 411015.
+                      </h4>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <FooterBottom />
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
--- src/components/Layout/Header/TopBar.js
+++ src/components/Layout/Header/TopBar.js
@@ -1,45 +1,47 @@
-import React, { Component } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
 
 const TopHeader = () => {
-
-    return (
-        <div className="header-top-area grey-bg d-none d-md-block">
-            <div className="container">
-                <div className="row">
-                    <div className="col-xl-8 col-lg-8 col-md-6 d-flex align-items-center">
-                        <div className="header-info">
-                            <span><i><FontAwesomeIcon icon={['far', 'map-marker-alt']} /></i> 15 Hamston Street, West</span>
-                            <span className="header-ph"><i><FontAwesomeIcon icon={['far', 'phone']} /></i> 812 (345) 6789</span>
-                            <span className="header-en"><i><FontAwesomeIcon icon={['far', 'envelope-open']} /></i> support@gmail.com</span>
-                        </div>
-                    </div>
-                    <div className="col-xl-4 col-lg-4 col-md-6">
-                        <div className="header-right f-right ">
-                            <div className="header-lang  pos-rel f-right">
-                                <div className="lang-icon">
-                                    <img src={require("../../../../public/assets/img/icon/flag.png")} alt="" />
-                                    <a href="">English <i><FontAwesomeIcon icon={['fal', 'angle-down']} /></i></a>
-                                </div>
-                                <ul className="header-lang-list">
-                                    <li><a href="#">USA</a></li>
-                                    <li><a href="#">UK</a></li>
-                                    <li><a href="#">CA</a></li>
-                                    <li><a href="#">AU</a></li>
-                                </ul>
-                            </div>
-                            <div className="header-icon f-right">
-                                <a href="#"><i><FontAwesomeIcon icon={['fab', 'facebook-f']} /></i></a>
-                                <a href="#"><i><FontAwesomeIcon icon={['fab', 'twitter']} /></i></a>
-                                <a href="#"><i><FontAwesomeIcon icon={['fab', 'instagram']} /></i></a>
-                                <a href="#"><i><FontAwesomeIcon icon={['fab', 'youtube']} /></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="header-top-area d-none d-lg-block">
+      <div className="container">
+        <div className="row">
+          <div className="col-xl-8 col-lg-8 col-md-8 col-sm-12 align-items-center">
+            <div className="header-info header-left d-none d-lg-block ">
+              <span className="header-ph">
+                <i>
+                  <FontAwesomeIcon icon={["far", "phone"]} />
+                </i>{" "}
+                <a href="tel:917620665635">+91 76206 65635</a>
+              </span>
+              <span className="header-en">
+                <i>
+                  <FontAwesomeIcon icon={["far", "envelope-open"]} />
+                </i>{" "}
+                <a href="mailto:sales@netcom-india.com">
+                  sales@netcom-india.com
+                </a>
+              </span>
             </div>
+          </div>
+          <div className="col-xl-4 col-lg-4 col-md-6 col-sm-12 align-items-center">
+            <div className="header-right d-none d-lg-block">
+              <Link href="coming-soon" as="coming-soon">
+                <a>Careers</a>
+              </Link>
+              <Link href="/blog" as="/blog">
+                <a>Blog</a>
+              </Link>
+              <Link href="/contact" as="/contact">
+                <a>Contact Us</a>
+              </Link>
+            </div>
+          </div>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default TopHeader;
